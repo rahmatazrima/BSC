207e15a55bf6f38d8cc6b24f1bd4c61cec03cc86
rahmatazrima <119133868+rahmatazrima@users.noreply.github.com>
2025-10-29 22:22:24 +0700

    fix logic for auth

--- src/app/(auth)/login/_components/login-form.tsx
+++ src/app/(auth)/login/_components/login-form.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { Controller, useForm } from "react-hook-form";
 import { schema, TLoginRequest } from "./data/type";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -8,6 +9,7 @@ import React, { useState, useEffect } from "react";
 import Link from "next/link";
 
 export default function LoginForm() {
+  const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
 
   const form = useForm<TLoginRequest>({
@@ -28,11 +30,17 @@ export default function LoginForm() {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(value),
+        credentials: "include",
       });
+      const data = await response.json();
 
-      if (response.ok) {
-        typeof window !== "undefined" && window.location.replace("/admin");
+      if (!response.ok) {
+        alert(data.message || "Login failed");
+        return;
       }
+
+      if (data.data.user.role === "ADMIN") router.replace("/admin");
+      else router.push("/booking");
     } catch (error) {
       console.error("Login error:", error);
     } finally {
